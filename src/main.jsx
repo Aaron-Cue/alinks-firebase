c70c177aab1aa7c112200a7d2f2e8861ee1f15a9
Aaron Cuellar <aaroncuellar2003@gmail.com>
2025-03-31 23:15:38 -0300

    basic routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App.jsx'
+import ChooseUsername from './routes/ChooseUsername.jsx'
+import Dashboard from './routes/Dashboard.jsx'
+import EditProfile from './routes/EditProfile.jsx'
+import Login from './routes/Login.jsx'
+import PublicProfile from './routes/PublicProfile.jsx'
+import SignOut from './routes/SignOut.jsx'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/dashboard/profile" element={<EditProfile />} />
+      <Route path="signout" element={<SignOut />} />
+      <Route path="u/:username" element={<PublicProfile />} />
+      <Route path="choose-username" element={<ChooseUsername />} />
+    </Routes>
+  </BrowserRouter>
 )
---
+++ src/routes/ChooseUsername.jsx
@@ -0,0 +1,3 @@
+export default function ChooseUsername() {
+  return <div>ChooseUsername</div>
+}

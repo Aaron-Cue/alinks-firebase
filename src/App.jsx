3f01c9dfe013831bd2f18c2f986e8ac5f2e8e646
Aaron Cuellar <aaroncuellar2003@gmail.com>
2025-04-11 15:54:05 -0300

    styles inicio & login

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { ToastContainer } from 'react-toastify'
 import AppRoutes from './routes/index'
 
 import 'react-toastify/dist/ReactToastify.css'
-import './App.css'
+import './index.css'
 
 function App() {
   return (
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,22 +1,44 @@
 import { useNavigate } from 'react-router-dom'
 
-export default function Inicio() {
+import '../styles/Inicio.css'
 
+export default function Inicio() {
   const navigate = useNavigate()
-  
+
   return (
     <>
-      <div>
-        Quiero mis A-links
-        <button
-          onClick={() => {
-            navigate('/login')
-          }}
-        >
-          Crear Cuenta
-        </button>
-      </div>
-      <h1>alinks App</h1>
+      <header className="header-inicio">
+        <div className="header-content">
+          <div className="logo">
+            <img src="src/assets/logo.png" alt="" />
+            A-Links
+          </div>
+          <button className="inicio-button" onClick={() => navigate('/login')}>
+            <span className="button_top"> Crear Cuenta </span>
+          </button>
+        </div>
+      </header>
+      <main className="container main-inicio">
+        <div className="inicio-content">
+          <h1 className="h1-inicio">
+            Coloque todos los enlaces necesarios en un solo lugar
+          </h1>
+          <p>
+            <i>Quiero obtener mis links</i>
+          </p>
+          <button
+            className="inicio-button"
+            onClick={() => {
+              navigate('/login')
+            }}
+          >
+            <span className="button_top"> Obtener </span>
+          </button>
+        </div>
+        <div className="screen">
+          <img src="/src/assets/imageApp.webp" alt="" />
+        </div>
+      </main>
     </>
   )
 }

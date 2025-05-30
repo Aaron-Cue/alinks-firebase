0484842df742bf2113273181db10b47e38c9520e
Aaron Cuellar <aaroncuellar2003@gmail.com>
2025-05-30 00:16:25 -0300

    fix error useAuth

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,5 +1,12 @@
 import { useContext } from 'react'
 import { AuthContext } from '../context/auth/Auth'
 
-const useAuth = () => useContext(AuthContext)
+const useAuth = () => { 
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error('useAuth error')
+  }
+  return context
+}
+  
 export default useAuth
